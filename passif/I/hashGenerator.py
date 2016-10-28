228dcbc6d003af7c5712965f685d86003504e226
Samuel Roy <samuel.roy07@gmail.com>
2016-10-28 13:07:30 -0400

    I

--- passif/I/hashGenerator.py
+++ passif/I/hashGenerator.py
@@ -31,7 +31,10 @@ class HashGenerator:
 
 if __name__ == "__main__":
 	hashGenerator = HashGenerator()
-	flag = sys.argv[1]
+	flaghash = cdb4a34ff782b0493874a40475f84ec958fb4541886d1e9be7c7ac70976107b1
+	for num in range(0,99999999999999):
+		if salt1 + num + salt2 == flaghash:
+			print(num)
 	if hashGenerator.validate(flag):
 		hash = hashGenerator.generateHash(flag)
 		print("the super secret hash is: " + hash)
